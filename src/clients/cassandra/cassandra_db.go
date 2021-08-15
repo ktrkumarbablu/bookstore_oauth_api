f6aee8d476da7c74dee9e97a4a4a86eead951049
Bablu Kumar <ktrkumarbablu@gmail.com>
2021-08-15 20:31:29 +0530

    add cassandra

---
+++ src/clients/cassandra/cassandra_db.go
@@ -0,0 +1,15 @@
+package cassandra
+
+import "github.com/gocql/gocql"
+
+func init() {
+	cluster := gocql.NewCluster("127.0.0.1")
+	cluster.Keyspace = "oauth"
+	cluster.Consistency = gocql.Quorum
+	session, err := cluster.CreateSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+}
